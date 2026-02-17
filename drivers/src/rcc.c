dd09839a02ca972829a975e23f2b9ea3769c09c4
Aswin P Krishnan <aswinpkrishnan94@gmail.com>
2026-02-17 14:59:00 +0530

    Create rcc.c

---
+++ drivers/src/rcc.c
@@ -0,0 +1,35 @@
+#include "rcc.h"
+
+void RCC_AHB1_CLK_EN()
+{
+}
+
+void RCC_AHB2_CLK_EN()
+{
+}
+
+void RCC_AHB3_CLK_EN()
+{
+}
+
+void RCC_AHB4_CLK_EN()
+{
+}
+
+void RCC_APB1_CLK_EN()
+{
+}
+
+void RCC_APB2_CLK_EN()
+{
+}
+
+void RCC_AHB1_CLK_EN()
+{
+}
+
+void RCC_AHB1_CLK_EN()
+{
+}
+
+
