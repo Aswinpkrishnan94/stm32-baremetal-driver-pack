13ddbc287a1a62dc1eabce248fe8cfe107fef1d9
Aswin P Krishnan <aswinpkrishnan94@gmail.com>
2026-02-17 16:11:02 +0530

    Create gpio.c

---
+++ drivers/src/gpio.c
@@ -0,0 +1,18 @@
+#include "gpio.h"
+
+void GPIO_Init(uint32_t *pGPIOx, uint8_t EnorDi)
+{
+}
+
+void GPIO_DeInit(uint32_t *pGPIOx, uint8_t EnorDi)
+{
+}
+void GPIO_WritePin(uint32_t* pGPIOx, uint16_t Pin_Number, uint8_t Pin_State)
+{
+}
+void GPIO_ReadPin(uint32_t* pGPIOx, uint16_t Pin_Number)
+{
+}
+void GPIO_TogglePin(uint32_t* pGPIOx,uint16_t Pin_Number)
+{
+}
