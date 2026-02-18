bb488360c3a1bb8b928fd64cf1e63c99a121543f
Aswin P Krishnan <aswinpkrishnan94@gmail.com>
2026-02-18 12:51:01 +0530

    Update rcc.c

--- drivers/src/rcc.c
+++ drivers/src/rcc.c
@@ -86,26 +86,3 @@ void RCC_APB4_CLK_DI(uint32_t mask)
 	RCC_APB4ENR &= ~(1 << mask);
 }
 
-void SystemClock_Config(void)
-{
-	RCC->CR |= RCC_CR_HSION;
-	while (!(RCC->CR & RCC_CR_HSIRDY));
-
-	    // Configure PLL1 for 480MHz (HCLK target)
-	    RCC->PLLCKSELR = (2U << 0);  // PLLSRC=HSI/2 = 32MHz
-	    RCC->PLL1DIVR = (1 << 0);    // DIVM1=1 (32MHz)
-	    RCC->PLL1FRACR = 0;          // FRACN1=0
-	    RCC->PLL1DIVR |= (79 << 8);  // DIVN1=79 (32*79=2528MHz)
-	    RCC->PLL1DIVR |= (2 << 19);  // DIVP1=2 (1264MHz)
-	    RCC->PLL1DIVR |= (4 << 16);  // DIVQ1=4 (632MHz)
-
-	    RCC->CR |= RCC_CR_PLL1ON;
-	    while (!(RCC->CR & RCC_CR_PLL1RDY));
-
-	    // Set system clocks
-	    RCC->CFGR = (0 << 0);        // SYSCLK=PLL1 (1264MHz/2? Wait HCLK mux)
-
-	    // Flash latency for 480MHz
-	    FLASH->ACR |= (7U << 0);     // 7 wait states
-}
-
