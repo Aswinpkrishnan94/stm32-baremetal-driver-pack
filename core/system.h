b0a6d11ae936fa53a154dd4d0ccd06776bcac41f
Aswin P Krishnan <aswinpkrishnan94@gmail.com>
2026-02-20 15:11:47 +0530

    Update system.h

--- core/system.h
+++ core/system.h
@@ -16,6 +16,13 @@
  * Called once from Reset_Handler before main()
  */
 
+/* Future Work:
+ * - PLL configuration
+ * - Cache enable (H7)
+ * - MPU setup
+ * - Clock tree configuration
+ */
+
 void System_Init(void);
 
 #endif /* INC_SYSTEM_H_ */
