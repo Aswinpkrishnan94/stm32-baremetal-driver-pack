2a073dd9765512ac304699eb2548521691eba7ea
Aswin P Krishnan <aswinpkrishnan94@gmail.com>
2026-02-20 15:02:09 +0530

    Create board.h

---
+++ bdp/board.h
@@ -0,0 +1,10 @@
+
+#ifndef INC_BOARD_H_
+#define INC_BOARD_H_
+
+void Board_Init(void);
+void Board_LED_ON(void);
+void Board_LED_OFF(void);
+void Board_LED_TOGGLE(void);
+
+#endif /* INC_BOARD_H_ */
